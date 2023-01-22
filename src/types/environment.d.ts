7aa1f59cb095855bd85cf95e3ca3334ce8d725b3
luiz-eduardo14 <--add>
2023-01-21 21:23:02 -0300

    feat: add config database env typescript

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,6 +3,11 @@ declare global {
     interface ProcessEnv {
       PUBLIC_KEY_MARVEL: string;
       PRIVATE_KEY_MARVEL: string;
+      DATABASE_PORT: number;
+      DATABASE_HOST: string;
+      DATABASE_USERNAME: string;
+      DATABASE_PASSWORD: string;
+      DATABASE_TYPE: 'postgres' | 'mysql'
     }
   }
 }
