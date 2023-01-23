5b74159be64aff80f312f83f57a3e4bacd240283
luiz-eduardo14 <--add>
2023-01-22 22:36:05 -0300

    feat:  change incorrect env database_password

--- src/characters/index.ts
+++ src/characters/index.ts
@@ -1,39 +1,39 @@
-import { Axios } from 'axios'
-import md5 from 'md5'
-import envFile from 'dotenv'
-import { writeFileSync, readFileSync } from 'fs'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import 'reflect-metadata'
-envFile.config()
-const _filename = fileURLToPath(import.meta.url)
-const _dirname = dirname(_filename)
+import { Axios } from 'axios';
+import md5 from 'md5';
+import envFile from 'dotenv';
+import { writeFileSync, readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import 'reflect-metadata';
+import { Hero } from 'types/marvel/character/Hero';
+envFile.config();
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 
 const marvelApi = new Axios({
   baseURL: 'http://gateway.marvel.com'
 
-})
+});
 
-const PRIVATE_KEY_MARVEL = process.env.PRIVATE_KEY_MARVEL
-const PUBLIC_KEY_MARVEL = process.env.PUBLIC_KEY_MARVEL
-const timestamp = Number(new Date())
+const PRIVATE_KEY_MARVEL = process.env.PRIVATE_KEY_MARVEL;
+const PUBLIC_KEY_MARVEL = process.env.PUBLIC_KEY_MARVEL;
+const timestamp = Number(new Date());
 
-const md5Hash = md5(String(timestamp) + PRIVATE_KEY_MARVEL + PUBLIC_KEY_MARVEL)
+const md5Hash = md5(String(timestamp) + PRIVATE_KEY_MARVEL + PUBLIC_KEY_MARVEL);
 
-console.log(md5Hash)
+console.log(md5Hash);
 
-const jsonFile = JSON.parse(readFileSync(_dirname + '/test.json').toString())
+const listHeroes: Hero[] = JSON.parse(readFileSync(_dirname + '/test.json').toString());
 
-const countStories = 0
+const countStories = 0;
 
-jsonFile.forEach((hero) => {
-  // countStories = countStories + hero.series.items.length;
-  console.log(hero?.thumbnail)
-})
+listHeroes.forEach((hero) => {
+  console.log(hero.name.trim());
+});
 
-console.log(jsonFile.length)
+console.log(listHeroes.length);
 
-console.log()
+console.log();
 
 // (async () => {
 //     const urlRequest = `/v1/public/characters?ts=${timestamp}&apikey=${PUBLIC_KEY_MARVEL}&hash=${md5Hash}`;
--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DATABASE_HOST ?? 'localhost',
   port: Number(process.env.DATABASE_PORT ?? '5432'),
   username: process.env.DATABASE_USERNAME ?? 'root',
-  password: process.env.PUBLIC_KEY_MARVEL ?? 'root',
+  password: process.env.DATABASE_PASSWORD ?? 'root',
   database: process.env.DATABASE_NAME,
   synchronize: true,
   logging: false,
