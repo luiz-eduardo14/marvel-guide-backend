ceb31b8b8f7abfb731c2cf88fbf61a125fb9a6b9
luiz-eduardo14 <--add>
2023-01-28 17:43:04 -0300

    feat: create script for population characters

--- src/scripts/character.ts
+++ src/scripts/character.ts
@@ -3,70 +3,85 @@ import md5 from 'md5';
 import envFile from 'dotenv';
 import { AppDataSource } from '../data-source.js';
 import { CharacterDataWrapper } from 'types/marvel/character/CharacterDataWrapper.js';
+import Character from '../entities/character.js';
+import SeriesSummary from '../entities/seriesSummary.js';
 envFile.config();
 
 (async () => {
   const database = await AppDataSource.initialize();
-  await database.runMigrations();
+  const migratiosList = await database.runMigrations({ transaction: 'all' });
 
-  console.log('success database');
+  if (migratiosList.length > 0) console.log('ALL MIGRATIONS WERE EXECUTED');
 
-  // const marvelApi = new Axios({
-  //   baseURL: 'http://gateway.marvel.com'
+  console.log('Database connections established');
 
-  // });
+  const marvelApi = new Axios({
+    baseURL: 'http://gateway.marvel.com'
+  });
 
-  // const PRIVATE_KEY_MARVEL = process.env.PRIVATE_KEY_MARVEL;
-  // const PUBLIC_KEY_MARVEL = process.env.PUBLIC_KEY_MARVEL;
-  // const timestamp = Number(new Date());
+  const PRIVATE_KEY_MARVEL = process.env.PRIVATE_KEY_MARVEL;
+  const PUBLIC_KEY_MARVEL = process.env.PUBLIC_KEY_MARVEL;
+  const timestamp = Number(new Date());
 
-  // const md5Hash: string = md5(String(timestamp) + PRIVATE_KEY_MARVEL + PUBLIC_KEY_MARVEL);
+  const md5Hash: string = md5(String(timestamp) + PRIVATE_KEY_MARVEL + PUBLIC_KEY_MARVEL);
 
-  // const urlRequest = `/v1/public/characters?ts=${timestamp}&apikey=${PUBLIC_KEY_MARVEL}&hash=${md5Hash}`;
+  const urlRequest = `/v1/public/characters?ts=${timestamp}&apikey=${PUBLIC_KEY_MARVEL}&hash=${md5Hash}`;
 
-  // const { data: json } = await marvelApi.get(`${urlRequest}&limit=1&offset=0`, {});
+  const { data: json } = await marvelApi.get(`${urlRequest}&limit=1&offset=0`, {});
 
-  // const resultRequestCharacter: CharacterDataWrapper = JSON.parse(json as string);
+  const resultRequestCharacter: CharacterDataWrapper = JSON.parse(json as string);
 
-  // if (resultRequestCharacter.code !== 200) {
-  //   throw new Error(resultRequestCharacter.status);
-  // }
+  if (resultRequestCharacter.code !== 200) {
+    throw new Error(resultRequestCharacter.status);
+  }
 
-  // const totalHeroes: number = resultRequestCharacter?.data?.total ?? 0;
+  const totalHeroes: number = resultRequestCharacter?.data?.total ?? 0;
 
-  // for (let index = 0; index < Math.ceil(totalHeroes / 100); index++) {
-  //   const offset = index === 0 ? index * 100 : (index * 100) + 1;
+  for (let index = 0; index < Math.ceil(totalHeroes / 100); index++) {
+    const offset = index === 0 ? index * 100 : (index * 100) + 1;
 
-  //   const { data: json } = await marvelApi.get(`${urlRequest}&limit=100&offset=${offset}`, {});
-  //   const { data }: CharacterDataWrapper = JSON.parse(json);
-  //   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  //   if (!data) {
-  //     throw new Error('Data not found');
-  //   }
-  // }
+    const { data: json } = await marvelApi.get(`${urlRequest}&limit=100&offset=${offset}`, {});
+    const { data }: CharacterDataWrapper = JSON.parse(json);
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!data) {
+      throw new Error('Data not found');
+    }
+
+    if (data.results === undefined || data.results === null) {
+      throw new Error('Results not found');
+    }
+
+    const defaultThumbnail = 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/default/explore-no-img';
+
+    for (const character of data.results) {
+      await database.createQueryBuilder().insert().into(Character).values([{
+        id_origin: character.id,
+        name: character.name,
+        description: character.description !== '' ? character.description : null,
+        modified: character.modified,
+        imageURL: `${character?.thumbnail?.path ?? defaultThumbnail}.${character?.thumbnail?.extension ?? 'jpg'}`,
+        resourceURI: character.resourceURI
+      }]).printSql().execute();
+      console.log(`\n${character.name} inserted`);
+
+      if (character.series === undefined || character.series === null) continue;
+
+      await database.createQueryBuilder()
+        .insert().into(SeriesSummary)
+        .values(character.series.items.map((item) => {
+          return {
+            id_origin: character.id,
+            name: item.name,
+            resourceURI: item.resourceURI
+          };
+        }))
+        .printSql().execute();
+      console.log(`\n${character.name} series inserted`);
+    }
+  }
 })().then(() => {
-  console.log('\nsuccess');
+  console.log('Characters insert with success');
 }).catch((error: Error) => {
-  console.log(error.message);
+  console.log(error?.message);
+  console.log('Error to insert characters');
 });
-
-// (async () => {
-//     const urlRequest = `/v1/public/characters?ts=${timestamp}&apikey=${PUBLIC_KEY_MARVEL}&hash=${md5Hash}`;
-
-//     const { data: json, } = await marvelApi.get(`${urlRequest}&limit=1&offset=0`,{})
-//     const totalHeroes: number = JSON.parse(json as string).data.total;
-
-//     const result = [];
-
-//     for (let index = 0; index < Math.ceil(totalHeroes / 100); index++) {
-//         const offset = index === 0 ? index * 100 : (index * 100) + 1;
-
-//         const { data: json, } = await marvelApi.get(`${urlRequest}&limit=100&offset=${offset}`,{})
-//         const { data } = JSON.parse(json);
-//         const resultsArray: any[] = data.results;
-
-//         // @ts-ignore
-//         result.push(...resultsArray)
-//     }
-//     writeFileSync(__dirname + '/test.json',JSON.stringify(result));
-// })();
