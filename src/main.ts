a38760dbaab9f82b1517252101d2d885e3bc9f84
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-03-15 23:47:00 -0300

    feat: change types in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { AppModule } from './app.module.js';
-const __dirname = fileURLToPath(dirname(import.meta.url));
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+await bootstrap();
