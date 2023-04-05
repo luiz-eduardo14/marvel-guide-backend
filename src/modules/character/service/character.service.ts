8d71abaff6db2459a266c0669e337a32a34f8cb8
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-04-04 21:45:49 -0300

    feat: create service of table character

---
+++ src/modules/character/service/character.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import CharacterEntity from '../../../entities/character.js';
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
+
+@Injectable()
+export class CharacterService {
+  constructor(
+    @InjectRepository(CharacterEntity)
+    private readonly characterRepository: Repository<CharacterEntity>,
+  ) {}
+
+  async paginate(
+    options: IPaginationOptions,
+  ): Promise<Pagination<CharacterEntity>> {
+    const queryBuilder = this.characterRepository.createQueryBuilder('c');
+    queryBuilder.orderBy('c.name', 'ASC'); // Or whatever you need to do
+    queryBuilder.where('c.description is not null');
+
+    return await paginate<CharacterEntity>(queryBuilder, options);
+  }
+
+  async findAll(): Promise<CharacterEntity[]> {
+    return await this.characterRepository
+      .createQueryBuilder('character')
+      .where('character.description is not null')
+      .getMany();
+  }
+
+  async findOne(id: number): Promise<CharacterEntity | null> {
+    return await this.characterRepository.findOne({ where: { id_origin: id } });
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.characterRepository.delete(id);
+  }
+}
