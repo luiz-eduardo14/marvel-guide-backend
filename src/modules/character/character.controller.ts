715cf0f0a16f412cc8983186491259807bf045e3
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-04-04 21:46:19 -0300

    feat: create controller for apply service characterService with endpoints

---
+++ src/modules/character/character.controller.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/return-await */
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import Character from 'src/entities/character.js';
+import { CharacterService } from './service/character.service.js';
+import { Pagination } from 'nestjs-typeorm-paginate';
+
+@Controller('character')
+export class CharacterController {
+  constructor(private readonly characterService: CharacterService) {}
+  @Get()
+  async getCharacter(): Promise<Character[]> {
+    return await this.characterService.findAll();
+  }
+
+  @Get('/paginate')
+  async paginate(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+  ): Promise<Pagination<Character>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.characterService.paginate({
+      page,
+      limit,
+    });
+  }
+}
